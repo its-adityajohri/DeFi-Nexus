6ed3f99bb043d0863bb48002f2508c9335d2d95b
Aditya <aj.adityajohri@gmail.com>
2023-10-20 07:44:59 +0530

    next-version updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link"
+import { Web3Button } from '@web3modal/react'
 
 export default function Home() {
-    return <Link href="/test">
-      <h1>Hello World</h1>
-    </Link>
+    return <>
+      <Link href="/test">
+        <h1>Hello World</h1>
+      </Link>
+    </>
 }
