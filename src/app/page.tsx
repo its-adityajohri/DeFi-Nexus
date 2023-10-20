cb57e6427898f59561bdf5d7491f82e0df2729ca
Aditya <aj.adityajohri@gmail.com>
2023-10-20 18:31:42 +0530

    next-updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import Link from "next/link"
-import { Web3Button } from '@web3modal/react'
+// import Link from "next/link"
+
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+// import Home from './Home/page';
+// import { useState } from 'react'
+
 
 export default function Home() {
-    return <>
-      <Link href="/test">
-        <h1>Hello World</h1>
-      </Link>
+
+    return(
+    <>
+      <header className="w-50">
+        <Navbar/>
+      </header>
+      <div className="w-[100vw] h-[100vh]">
+      <div className="flex flex-col justify-center items-center p-48 gap-9">
+        <h1 className="text-6xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+        <p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A similique iusto eligendi deserunt distinctio minus ad quam eius ipsum debitis obcaecati esse neque quod delectus officiis, nesciunt tenetur quia, quibusdam iste? Eum expedita omnis a aspernatur explicabo rem aliquid dolore amet hic tenetur similique, et perferendis quidem incidunt? Est, earum.</p>
+        <button className="hover:bg-transparent outline outline-2 p-2 m-2 border-none text-xl font-semibold outline-black rounded-md bg-black text-white hover:text-black">Get Started</button>
+      </div>
+    </div>
     </>
+  )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+// "use client"
+
+import Link from 'next/link';
+// import React, { useState } from 'react'
+import WalletConnect from './WalletConnect';
+// import { Dialog } from '@headlessui/react'
+// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+// import { Web3Button } from '@web3modal/react'
+
+const navigation = [
+  { name: 'Loans', href: '/loans' },
+  { name: 'Docs', href: '/docs' },
+  { name: 'Vote', href: '/vote' },
+  { name: 'Swap', href: '/swap' },
+];
+
+
+const Navbar = () => {
+  return (
+    <nav className="relative mt-5 flex items-center justify-between px-8 text-xl font-semibold" >
+      <div className='flex-1 pt-1 font-bold leading-6 text-gray-700'>
+        <Link href='/'>DEFI-Nexus</Link>
+      </div>
+
+      <div className="flex gap-x-12">
+        {navigation.map((item) => (
+          <a key={item.name} href={item.href} className="leading-6 text-gray-900">
+            {item.name}
+          </a>
+        ))}
+      </div>
+
+      <div className="flex flex-1 justify-end">
+        <Link href='/dashboard'>
+          <WalletConnect/>
+        </Link>
+      </div>
+      <hr className='absolute top-14 left-[5%] w-[90%] p-[5px]' />
+    </nav>
+)
+}
+
+export default Navbar
