d545828bb3b64fe575a65d77de922849c3d7b481
Aditya <aj.adityajohri@gmail.com>
2023-10-21 18:45:19 +0530

    P2P Swap UI Updates

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -1,9 +1,312 @@
-import React from 'react'
+"use client"
+import React from "react";
+
+const transactions=[
+  {
+    id:1,
+    name:"SparkFun",
+    amount:"89.4 USD",
+  },
+  {
+    id:1,
+    name:"SparkFun",
+    amount:"89.4 USD",
+  },
+  {
+    id:1,
+    name:"SparkFun",
+    amount:"89.4 USD",
+  },
+  {
+    id:1,
+    name:"SparkFun",
+    amount:"89.4 USD",
+  },
+  {
+    id:1,
+    name:"SparkFun",
+    amount:"89.4 USD",
+  },
+]
 
 const Swap = () => {
   return (
-    <div>Swap</div>
-  )
-}
+    // <div className='p-10 m-10 flex mr-20'>
+    //   <div className='flex-1'>
+    //     <div className='flex items-center justify-around'>
+    //       <span>
+    //         <h2 className="text-xl font-semibold">SparkFun</h2>
+    //         <p className="max-w-[120px] text-md overflow-hidden">0x8fb39fe46d3dfes4vkels94ks0wkf</p>
+    //       </span>
+    //       <h2 className="text-xl font-semibold">0.01 DAI</h2>
+    //     </div>
+    //     <div className='relative h-full'>
+    //       {/*  */}
+    //     </div>
+    //   </div>
 
-export default Swap
+    //   <div className='flex-1'>
+    //     <div className='flex items-center justify-between'>
+    //       <span>
+    //         <h2 className="text-xl font-semibold">Plaid x BlockChain</h2>
+    //         <p className="text-md overflow-hidden">Select a payment to verify it on-chain</p>
+    //       </span>
+    //       <button className="text-xl font-semibold">LogOut</button>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="p-10 m-10 flex mr-20 gap-10 h-[90vh] justify-evenly">
+      <div className="flex-1 flex flex-col p-10 bg-slate-300/70 rounded-xl max-w-[500px] max-h-[full]">
+        <div className="flex flex-row items-center justify-around w-full p-2">
+          <div className="flex-1">
+            <h2 className="text-xl font-semibold">UserName</h2>
+            <p className="text-md">Select Transaction to verify it on chain</p>
+          </div>
+          <button className="p-2 m-2 bg-black hover:bg-white text-white hover:text-black font-semibold rounded-lg outline outline-black outline-2">LogOut</button>
+        </div>
+        <div className="flex flex-col mt-8">
+          <div className="flex flex-col p-2 m-2 gap-5 max-h-[350px] overflow-y-auto no-scrollbar">
+            {
+              transactions.map((item)=>(
+                <div className="border-solid border-2 border-gray-500 rounded-xl w-full p-5 flex justify-between items-center" >
+                  <span>
+                    <h2 className="font-semibold">{item.name}</h2>
+                    <p>{item.amount}</p>
+                  </span>
+                  <input type="radio"  className="h-5 w-5 cursor-pointer" onClick={()=>{}}></input>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <div className="flex flex-col flex-1 bg-slate-300/70 max-w-[600px] max-h-[700px] p-10 rounded-xl">
+        <div className="">
+          <h1 className="text-xl overflow-scroll no-scrollbar font-semibold max-w-[350px]">
+            0x0s45wgk456dkght673eht6esdpotv9w4942jiwrij42dsjvbj49dsv9e3939f3r9
+            </h1>
+            <p>0.01 DAI</p>
+            <hr className="p-[0.5px] m-0 bg-black"/>
+        </div>
+
+        <div className="flex flex-col flex-auto h-full p-4">
+          <div className="flex flex-col h-full overflow-x-auto mb-4">
+            <div className="flex flex-col h-full">
+              <div className="grid grid-cols-12 gap-y-2">
+                <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                  <div className="flex flex-row items-center">
+                    <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                      A
+                    </div>
+                    <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                      <div>Hey How are you today?</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                  <div className="flex flex-row items-center">
+                    <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                      A
+                    </div>
+                    <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                      <div>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Vel ipsa commodi illum saepe numquam maxime
+                        asperiores voluptate sit, minima perspiciatis.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-start-6 col-end-13 p-3 rounded-lg">
+                  <div className="flex items-center justify-start flex-row-reverse">
+                    <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                      A
+                    </div>
+                    <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                      <div>I'm ok what about you?</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-start-6 col-end-13 p-3 rounded-lg">
+                  <div className="flex items-center justify-start flex-row-reverse">
+                    <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                      A
+                    </div>
+                    <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                      <div>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing. ?
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                  <div className="flex flex-row items-center">
+                    <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                      A
+                    </div>
+                    <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                      <div>Lorem ipsum dolor sit amet !</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-start-6 col-end-13 p-3 rounded-lg">
+                  <div className="flex items-center justify-start flex-row-reverse">
+                    <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                      A
+                    </div>
+                    <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                      <div>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing. ?
+                      </div>
+                      <div className="absolute text-xs bottom-0 right-0 -mb-5 mr-2 text-gray-500">
+                        Seen
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                  <div className="flex flex-row items-center">
+                    <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                      A
+                    </div>
+                    <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                      <div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Perspiciatis, in.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                  <div className="flex flex-row items-center">
+                    <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                      A
+                    </div>
+                    <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                      <div className="flex flex-row items-center">
+                        <button className="flex items-center justify-center bg-indigo-600 hover:bg-indigo-800 rounded-full h-8 w-10">
+                          <svg
+                            className="w-6 h-6 text-white"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="1.5"
+                              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                            ></path>
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="1.5"
+                              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                            ></path>
+                          </svg>
+                        </button>
+                        <div className="flex flex-row items-center space-x-px ml-4">
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-4 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-12 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-6 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-5 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-4 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-3 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-1 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-1 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                          <div className="h-4 w-1 bg-gray-500 rounded-lg"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-row items-center h-16 rounded-xl mb-5 bg-white w-full px-4">
+            <div>
+              {/* <button className="flex items-center justify-center text-gray-400 hover:text-gray-600">
+                <svg
+                  className="w-5 h-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                  ></path>
+                </svg>
+              </button> */}
+            </div>
+            
+            <div className="flex-grow ml-4">
+              <div className="relative w-full">
+                <input
+                  type="text"
+                  className="flex w-full border rounded-xl focus:outline-none focus:border-black pl-4 h-10"
+                />
+              </div>
+            </div>
+
+            <div className="ml-4">
+              <button className="flex items-center justify-center bg-black hover:bg-white rounded-xl px-3 py-1 outline text-white hover:text-black outline-2 outline-black">
+                <span>Send</span>
+                <span className="ml-2">
+                  <svg
+                    className="w-4 h-4 transform rotate-45 -mt-px"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                    ></path>
+                  </svg>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Swap;
